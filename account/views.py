5fac510279b911fa7a5e1945e3271ea18f13954c
Marwan Saad <marwan10salem@hotmail.com>
2020-05-18 09:16:43 +0200

    user login logout  register completed

--- account/views.py
+++ account/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .forms import UserForm, ProfileForm
+from .models import Account
 # Create your views here.
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('/movies')
+    if request.method == 'POST':
+        userform = UserForm(request.POST)
+        profileform = ProfileForm(request.POST)
+        if userform.is_valid() and profileform.is_valid():
+            user = userform.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profileform.save(commit=False)
+            profile.user = user
+            if 'profile_picture' in request.FILES:
+                profile.pic = request.FILES['profile_picture']
+            profile.save()
+            return redirect('/movies')
+        else:
+            print("Error",userform.errors," Profile forms",profileform.errors)
+    else:
+        userform = UserForm()
+        profileform = ProfileForm()
+    return render(request,'register.html', {'userform':userform,
+    'profileform':profileform})
